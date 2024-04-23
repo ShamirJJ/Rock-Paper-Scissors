195475e7e7a6f759ac3f5449ff4a32a4df8e98d2
ShamirJJ <shamir_j@hotmail.com>
2024-04-24 00:16:04 +0100

    Updated HTML code and added in randomised computer selection code

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //javascript code placeholder
+console.log("Hello World");
+
+function getComputerChoice () {
+    let computerResult = Math.floor(Math.random() * 100) + 1;
+    if (computerResult <=33) {
+        console.log("Rock");
+        
+    } else if (computerResult > 33 && computerResult < 66) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
+}
+
+getComputerChoice(Math.floor(Math.random()*100) +1);
+
+console.log(getComputerChoice);
